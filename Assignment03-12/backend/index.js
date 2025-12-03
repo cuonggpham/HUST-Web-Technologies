aad58b61208c4f28587124c0fe8055b59537707d
cuonggpham <qcuonghls@gmail.com>
2025-12-03 20:16:40 +0700

    init 03-12

---
+++ Assignment03-12/backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
